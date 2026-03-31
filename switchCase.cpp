d6e8fce2369804110ce6c10c6624afe2f8d24705
ayamtulebo19@gmail.com <Ayamtulebo19@gmail.com>
2026-03-31 16:55:31 +0700

    Menambahkan prosedur output hasil seleksi jurusan

--- switchCase.cpp
+++ switchCase.cpp
@@ -39,5 +39,14 @@ void pilihanJurusan() {
     }
 }
 
+// prosedur output
+void output() {
+    cout << "\n=== HASIL SELEKSI ===" << endl;
+    cout << "Jurusan Pilihan : " << namaJurusan << endl;
+    cout << "Status Kuota    : " << statusKuota << endl;
+    cout << "----------------------------------" << endl;
+}
+
+
 
 
